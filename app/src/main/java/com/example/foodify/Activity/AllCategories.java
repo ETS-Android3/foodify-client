35a1d3af201e7c563a8c1a4ca8e65df8670cdcf3
Jatin <jatinmangla2001@gmail.com>
2021-11-27 07:03:46 +0530

    Navigation Drawer

--- app/src/main/java/com/example/foodify/Activity/AllCategories.java
+++ app/src/main/java/com/example/foodify/Activity/AllCategories.java
@@ -1,10 +1,19 @@
 package com.example.foodify.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.foodify.Adapter.CategoryAdapter;
@@ -12,6 +21,7 @@ import com.example.foodify.Model.Category;
 import com.example.foodify.R;
 import com.example.foodify.Retrofit.NetworkClient;
 import com.example.foodify.Retrofit.RetrofitInterface;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -20,11 +30,12 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
-public class AllCategories extends AppCompatActivity {
-
+public class AllCategories extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    ActionBarDrawerToggle drawerToggle;
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     private RetrofitInterface service;
-
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
     RecyclerView recycler_menu;
     RecyclerView.LayoutManager layoutManager;
 
@@ -32,11 +43,23 @@ public class AllCategories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_categories);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        toolbar=findViewById(R.id.toolbar);
 
+//        toolbar.setTitle("Menu");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         recycler_menu=findViewById(R.id.recycler_category);
         recycler_menu.setHasFixedSize(true);
         layoutManager=new LinearLayoutManager(this);
         recycler_menu.setLayoutManager(layoutManager);
+        drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.setDrawerListener(drawerToggle);
 
 
         Retrofit retrofitClient = NetworkClient.getInstance();
@@ -63,4 +86,27 @@ public class AllCategories extends AppCompatActivity {
             recycler_menu.setAdapter(new CategoryAdapter(categories));
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
